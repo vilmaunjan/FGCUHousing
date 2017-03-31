1f3cdfd4502fb10afa5bcd8abfa5ec25d54cf26d
calebheinzman <caleb.heinzman@gmail.com>
2017-03-31 03:24:54 -0400

    What I did: I got the register, and login features to work. I created a user class to store infromation when a user registers.
    Still need to do:
    Update User, and text fields to include fName, lName, and UIN.
    Have User check for invalid inputs such as improper email, and invalid password.

---
+++ app/src/main/java/com/example/vilma/fgcuhousing/data/User.java
@@ -0,0 +1,51 @@
+package com.example.vilma.fgcuhousing.data;
+
+/**
+ * Created by calebheinzman on 3/30/17.
+ * This class is used to store the user information after registration to be inserted in the database.
+ */
+
+public class User {
+    private String name, email, password, building;
+    private boolean validInput;
+
+    public boolean isValidInput() {
+        return validInput;
+    }
+
+    public void setValidInput(boolean validInput) {
+        this.validInput = validInput;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+}
