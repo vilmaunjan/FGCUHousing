da9a707d1b2e11aec4e7188bd0384cf7709f38cf
calebheinzman <caleb.heinzman@gmail.com>
2017-03-31 17:52:26 -0400

    What I did: I got the register, and login features to work. I created a user class to store infromation when a user registers.
    Still need to do:
    Update User, and text fields to include fName, lName, and UIN.
    Have User check for invalid inputs such as improper email, and invalid password.

--- app/src/main/java/com/example/vilma/fgcuhousing/data/DbHandler.java
+++ app/src/main/java/com/example/vilma/fgcuhousing/data/DbHandler.java
@@ -22,7 +22,7 @@ public class DbHandler extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String Tag = "DataBaseHelper";
     SQLiteDatabase db;
-
+    int num;
     public DbHandler(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
