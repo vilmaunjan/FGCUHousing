5c63db5c5147e02c9b6dba375efd433ada8699e6
JtdelBagno <jtdelbagno@gmail.com>
2017-04-02 15:18:24 -0400

    Resident Input Verification
    Added basic input verification to the resident registration screen. Some
    kind of pop-up or text needs to appear to tell the user to reenter info
    in that feild.

--- app/src/main/java/com/example/vilma/fgcuhousing/ResidentRegister.java
+++ app/src/main/java/com/example/vilma/fgcuhousing/ResidentRegister.java
@@ -25,7 +25,9 @@ public class ResidentRegister extends AppCompatActivity {
     private EditText Name;
     private EditText Email;
     private EditText Password;
+    private EditText PasswordVerify;
     private AutoCompleteTextView Building;
+    private boolean validInput = true;
 
     DbHandler db = new DbHandler(this);
 
@@ -59,37 +61,58 @@ public class ResidentRegister extends AppCompatActivity {
     public void buttonOnClick(View v) {
         Button button = (Button)v;
         if (v == findViewById(R.id.btnSubmit)) {
-            //validate entry values and go to residentLogin
-            //Still working on validating the entry's
+
             Name = (EditText) findViewById(R.id.txtName);
             Email = (EditText) findViewById(R.id.txtEmail);
             Password = (EditText) findViewById(R.id.txtPassword);
+            PasswordVerify = (EditText) findViewById(R.id.txtPasswordVerify);
             Building = (AutoCompleteTextView) findViewById(R.id.buildings_list);
 
             //gets the paramater from the android screen and convert to string
             String nameEntry = Name.getText().toString();
             String emailEntry = Email.getText().toString();
             String passwordEntry = Password.getText().toString();
+            String passwordVerify = PasswordVerify.getText().toString();
             String buildingEntry = Building.getText().toString();
 
+            //not sure what this is
             Log.d("Reg" , "Ther name entered is " + nameEntry);
             Log.d("Reg" , "Ther Email entered is " + emailEntry);
             Log.d("Reg" , "Ther Password entered is " + passwordEntry);
             Log.d("Reg" , "Ther Building entered is " + buildingEntry);
 
+            //validate info here
+            //name must contain only letters and space
+            if(validInput == true){
+                validInput = nameEntry.matches("[a-zA-Z]+ ");
+            }
+            //email must be in format ****@eagle.fgcu.edu
+            //in full version must verify email though an Email.
+            if(validInput == true) {
+                validInput = emailEntry.endsWith("@eagle.fgcu.edu");
+            }
+            //passwords must match
+            if(validInput == true) {
+                validInput = passwordEntry.equals(passwordVerify);
+            }
 
 
-            //Inserts user information into user class.
-            User usr = new User();
-            usr.setName(nameEntry);
-            usr.setEmail(emailEntry);
-            usr.setPassword(passwordEntry);
-            usr.setBuilding(buildingEntry);
+            if(validInput == true) {
+                //Inserts user information into user class.
+                User usr = new User();
+                usr.setName(nameEntry);
+                usr.setEmail(emailEntry);
+                usr.setPassword(passwordEntry);
+                usr.setBuilding(buildingEntry);
 
-            //Inserts New User into the database
-            db.insertUser(this,usr);
-            //Moves to next Activity
-            startActivity(new Intent(getApplicationContext(), ResidentLogin.class));
+                //Inserts New User into the database
+                db.insertUser(this, usr);
+                //Moves to next Activity
+                startActivity(new Intent(getApplicationContext(), ResidentLogin.class));
+            }
+            else{
+                //popup with "'feild' is invalid"
+            }
         }
     }
 
