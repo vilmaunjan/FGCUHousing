dad2fa0d7bbd9f4d61e6e1510329f401ed85eb11
Andrew Charles <andrewmcharles@live.com>
2017-04-14 13:30:20 -0400

    Working on Alot

---
+++ app/src/main/java/com/example/vilma/fgcuhousing/AwardInfo.java
@@ -0,0 +1,20 @@
+package com.example.vilma.fgcuhousing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Andre on 4/13/2017.
+ */
+
+public class AwardInfo extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.awardwindow);
+
+        int p = 1;
+    }
+}
