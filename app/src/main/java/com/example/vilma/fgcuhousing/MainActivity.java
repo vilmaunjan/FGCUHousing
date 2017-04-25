c96712c0b1c01da244f37838277302644cbb9fe4
vilmaunjan <vrunjan0014@eagle.fgcu.edu>
2017-04-25 00:00:29 -0400

    Removed main menu from mainActivity, cause is not used.

--- app/src/main/java/com/example/vilma/fgcuhousing/MainActivity.java
+++ app/src/main/java/com/example/vilma/fgcuhousing/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public void imageOnClick(View i){
         if(i == findViewById(R.id.StarMode)) {
             try {
@@ -68,30 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Admin", e.getMessage());
             }
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
